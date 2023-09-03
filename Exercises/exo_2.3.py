b07cdeb03d611f05b3288451b9d835c4de81dbe1
GWartelle <115559211+GWartelle@users.noreply.github.com>
2023-09-03 17:01:44 +0200

    Delete Exercises directory

--- Exercises/exo_2.3.py
+++
@@ -1,6 +0,0 @@
-# This first line is provided for you
-
-hrs = input("Enter Hours:")
-rat = input("Enter Rate:")
-pay = float(hrs)*float(rat)
-print("Pay:", pay)
--- Exercises/exo_6.5.py
+++
@@ -1,4 +0,0 @@
-text = "X-DSPAM-Confidence:    0.8475"
-nbr_place = text.find("0.")
-nbr = float(text[nbr_place:])
-print(nbr)
